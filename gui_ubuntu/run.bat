355bffffa30a1d39a3f620b506a6f539c25df84f
Irza Pulungan <irzaip@gmail.com>
2018-10-25 17:26:20 +0700

    GUI UBUNTU UPDATE

--- gui_ubuntu/run.bat
+++ gui_ubuntu/run.bat
@@ -1,3 +1,3 @@
 docker build -t firefox .
-set-variable -name DISPLAY -value 127.0.0.1:0.0
-docker run -ti --rm -e DISPLAY=$DISPLAY firefox
+set -name DISPLAY -value 127.0.0.1:0.0
+docker run -ti --rm -e DISPLAY=192.168.88.67:0.0 firefox
